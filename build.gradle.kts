327e62d704163eab67b03b9d79543f47a5a0f4b6
Dabu Zhang <15770165+superPershing@users.noreply.github.com>
2020-07-29 10:16:52 +0800

    feat: init EurekaServer

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.3.2.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
+}
+
+group = "com.thoughtworks.archguard"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server:2.2.3.RELEASE")
+}
+
+configure<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension> {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:Hoxton.SR6")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
